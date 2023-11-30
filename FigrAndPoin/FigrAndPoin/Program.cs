54a5dc3700779e81fb2ae04afc7bb9e6aef12ba2
ArtemiyFedorenko <NTKshnik.247@gmail.com>
2023-11-30 15:34:00 +0300

    Download

---
+++ FigrAndPoin/FigrAndPoin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+class Program
+{
+    public static void Main()
+    {
+        Point A = new Point(1, 2, "A");
+        Point B = new Point(3, 4, "B");
+        Point C = new Point(5, 6, "C");
+
+        if (IsValidTriangle(A, B, C))
+        {
+            Figure triangle = new Figure("Triangle", new Point[] { A, B, C });
+            triangle.CalculatePerimeter();
+        }
+        else
+        {
+            Console.WriteLine("Invalid triangle: Not enough or too many points.");
+        }
+    }
+
+    public static bool IsValidTriangle(Point A, Point B, Point C)
+    {
+        return !(A == B || A == C || B == C);
+    }
+}
---
+++ SecApp/SecApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double side1, side2;
+
+        do
+        {
+            Console.WriteLine("Enter the length of side1: ");
+        } while (!double.TryParse(Console.ReadLine(), out side1) || side1 <= 0);
+
+        do
+        {
+            Console.WriteLine("Enter the length of side2: ");
+        } while (!double.TryParse(Console.ReadLine(), out side2) || side2 <= 0);
+
+        Rectangle myRectangle = new Rectangle(side1, side2);
+
+        Console.WriteLine($"Perimeter: {myRectangle.Perimeter}");
+        Console.WriteLine($"Area: {myRectangle.Area}");
+    }
+}
